d4e6948054f30b6a1c63babc9f6fea2ac076c84a
Serhii Chaban <serhii@scanbot.io>
2020-02-07 18:13:39 +0100

    for support libraries sdk

--- camera/src/main/java/com/commonsware/cwac/camera/CameraFragment.java
+++ camera/src/main/java/com/commonsware/cwac/camera/CameraFragment.java
@@ -17,13 +17,13 @@ package com.commonsware.cwac.camera;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import java.io.IOException;
 
-import androidx.fragment.app.Fragment;
 
 /**
  * Primary class for using `CameraView` as a fragment. Just
